da62fe0370c5e53f63a166cbbe93f761bfb6cafd
Dide Mertens <d.g.j.mertens@gmail.com>
2019-04-21 15:33:32 +0100

    made simple project with topics and entries

---
+++ django/11_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/DideVeldhuijzen/code/didemertens/Python_Crash_Course/django/11_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
